bab0e3216db0ed69bde3f4cfa32f165fd3f1ae30
Alamin2017 <alamincse12@gmail.com>
2025-10-08 23:34:54 +0600

    updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,9 +27,9 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['allure-playwright'],
-  ],
+  // reporter: [
+  //   ['allure-playwright'],
+  // ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   timeout: 60000,
   use: {
