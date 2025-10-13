2923844f9f5eed9bd207382b7dc11962ba9f9f7f
Alamin2017 <alamincse12@gmail.com>
2025-10-14 00:57:38 +0600

    updated

--- utils/global-setup.ts
+++ utils/global-setup.ts
@@ -6,7 +6,8 @@ import { chromium, FullConfig} from '@playwright/test';
 async function globalSetup(config: FullConfig) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.saucedemo.com/');
+  const baseURL = config.projects[0].use.baseURL;
+  await page.goto(baseURL!);
   await page.fill("//input[@id='user-name']", 'standard_user');
   await page.fill("//input[@id='password']", 'secret_sauce');
   await page.click("//input[@id='login-button']");
