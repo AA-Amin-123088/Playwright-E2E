2ae6e60407e678cc5dc6c28b6dea9923140cdecb
Alamin2017 <alamincse12@gmail.com>
2025-10-14 22:33:41 +0600

    update

--- utils/global-setup.ts
+++ utils/global-setup.ts
@@ -1,5 +1,5 @@
 // global-setup.ts
-
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
 import { chromium, FullConfig} from '@playwright/test';
 
@@ -7,10 +7,11 @@ async function globalSetup(config: FullConfig) {
   const browser = await chromium.launch();
   const page = await browser.newPage();
   const baseURL = config.projects[0].use.baseURL;
-  await page.goto(baseURL!);
+  await page.goto(`${baseURL}`);
   await page.fill("//input[@id='user-name']", 'standard_user');
   await page.fill("//input[@id='password']", 'secret_sauce');
   await page.click("//input[@id='login-button']");
+  await delay(2000);
   await page.context().storageState({ path: 'auth.json' });
 }
 export default globalSetup;
