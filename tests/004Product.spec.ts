a1743995ab689b4a7793e8a62e6f8da952b36b92
Alamin2017 <alamincse12@gmail.com>
2026-01-10 17:12:26 +0600

    updated

--- tests/004Product.spec.ts
+++ tests/004Product.spec.ts
@@ -1,21 +1,21 @@
-import { test,chromium,Page } from '@playwright/test';
-const delay = (ms: number | undefined) => new Promise(resolve => setTimeout(resolve, ms));
+// import { test,chromium,Page } from '@playwright/test';
+// const delay = (ms: number | undefined) => new Promise(resolve => setTimeout(resolve, ms));
 
-test.describe.serial('TEST SUITE', async () => {
-  let page: Page;
-  test.beforeAll("Browser is open without fixture",async()=>{
-      const browser=await chromium.launch();
-      const context=await browser.newContext();
-      page=await context.newPage();
-      await page.goto("https://www.nop-station.com/");
-  })
-  test.afterAll("Browser is closed without fixture",async()=>{
-      page.close();
-  })
-  test('Without-fixtures 3', async () => {
-      await page.locator('//*[@id="allow-push-notification"]').click();;
-    });
-  test('Without-fixtures 4', async () => {
-      await page.locator("//a[@class='ico-cart']").click();
-    });
-});
+// test.describe.serial('TEST SUITE', async () => {
+//   let page: Page;
+//   test.beforeAll("Browser is open without fixture",async()=>{
+//       const browser=await chromium.launch();
+//       const context=await browser.newContext();
+//       page=await context.newPage();
+//       await page.goto("https://www.nop-station.com/");
+//   })
+//   test.afterAll("Browser is closed without fixture",async()=>{
+//       page.close();
+//   })
+//   test('Without-fixtures 3', async () => {
+//       await page.locator('//*[@id="allow-push-notification"]').click();;
+//     });
+//   test('Without-fixtures 4', async () => {
+//       await page.locator("//a[@class='ico-cart']").click();
+//     });
+// });
