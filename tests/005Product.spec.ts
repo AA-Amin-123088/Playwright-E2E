55e13f0e98e355986c5143c3a288b8ef2142688b
Alamin2017 <alamincse12@gmail.com>
2026-04-07 20:24:34 +0600

    updated

--- tests/005Product.spec.ts
+++ tests/005Product.spec.ts
@@ -1,26 +1,4 @@
-// import { test, request, expect, chromium, Page, APIRequestContext } from '@playwright/test';
-// import { PageObject } from '../pageobject/PageObject';
-// import { faker } from '@faker-js/faker';
-// const randomEmail = faker.internet.email();
-// test.describe.serial('TEST CASE 1-Validate registration using valid data:-', async () => {
-//   let page: Page;
-//   test.beforeAll("Browser is open without fixture",async()=>{
-//       const browser=await chromium.launch();
-//       const context=await browser.newContext();
-//       page=await context.newPage();
-//   })
-//   test.afterAll("Browser is closed without fixture",async()=>{
-//       page.close();
-//   })
-//   test('Open current browser and disable notifications', async () => {
-//     const ObjectManager=new PageObject(page);
-//     await page.goto("https://automationexercise.com/");
-//     await expect(ObjectManager.homeObj.home_text_header()).toHaveText("Home");
-//     await ObjectManager.homeObj.product_link().click();
-//     await ObjectManager.homeObj.add_to_cart_button().click();
-//     await ObjectManager.homeObj.view_cart_link().click();
-//     });
-// });
+// import { test, request, APIRequestContext } from '@playwright/test';
 
 // let reqContext2: APIRequestContext;
 // test.beforeAll("Before all the test", async () => {
@@ -50,38 +28,3 @@
 //   const resp2=await reqContext.get("/booking");
 //   console.log(await resp2.json());
 // });
-// test("Practice-3", async ({request}) => {
-//   const resp4 = await request.get("/booking/641");
-//   console.log(await resp4.json());
-// });
-// test("Practice-4", async ({request}) => {
-//   const resp5 = await request.get("/booking?firstname=Jim&lastname=Brown");
-//   console.log(await resp5.json());
-// });
-// test("Practice-5", async ({request}) => {
-//   const resp6 = await request.get("/booking",{
-//     params:{
-//       firstname:"Josh",
-//       lastname:"Allen"
-//     }
-//   });
-//   console.log(await resp6.json());
-
-// });
-
-// test("Practice-Post-6", async ({request}) => {
-//   const resp7 = await request.post("/booking",{
-//     data:{
-//       firstname: 'Jim11',
-//       lastname: 'brown11',
-//       totalprice: 11111,
-//       depositpaid: true,
-//       bookingdates: { checkin: '2018-11-11', checkout: '2019-11-11' },
-//       additionalneeds: 'midnight snack111'
-//     }
-//   });
-//   const jsonResponse1=await resp7.json();
-//   console.log(jsonResponse1);
-// });
-
-//update
